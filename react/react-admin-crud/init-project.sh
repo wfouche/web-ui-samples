48a1181a3513c178e56299507226af4281aac56a
werner.fouche@gmail.com <0004edb40420>
2020-12-14 09:53:03 +0200

    Added missing modules.

--- react/react-admin-crud/init-project.sh
+++ react/react-admin-crud/init-project.sh
@@ -4,6 +4,9 @@ cd test-admin
 
 npx yarn add react-admin ra-data-json-server prop-types ra-data-fakerest recharts
 
+npx yarn add axios
+npx yarn add @raphiniert/ra-data-postgrest
+
 cp  ../scripts/*  src
 
 npx yarn start
