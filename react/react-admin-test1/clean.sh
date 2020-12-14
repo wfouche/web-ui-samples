326157a42e5c934c42f999bb63e454235c3240e0
Werner Fouché <werner.fouche@aciworldwide.com>
2020-12-14 01:07:40 -0500

    Scripts to build and run the app on Linux.

---
+++ react/react-admin-test1/clean.sh
@@ -0,0 +1,1 @@
+rm -f -r test-admin/ yarn.lock package.json node_modules/
---
+++ react/react-admin-test1/init-project.sh
@@ -0,0 +1,9 @@
+npx create-react-app test-admin
+
+cd test-admin
+
+npx yarn add react-admin ra-data-json-server prop-types ra-data-fakerest recharts
+
+cp  ../scripts/*  src
+
+npx yarn start
